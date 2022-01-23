2a9c3c175285015ce1c33a784a53b65316b154ec
maxcore25 <mkorobov2002@mail.ru>
2022-01-23 09:45:55 +0300

    Cursor Animation done

---
+++ app.js
@@ -0,0 +1,20 @@
+let mouseCursor = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll('.nav-links li');
+
+window.addEventListener('mousemove', cursor);
+
+function cursor(e) {
+  mouseCursor.style.top = e.pageY + 'px';
+  mouseCursor.style.left = e.pageX + 'px';
+}
+
+navLinks.forEach(link => {
+  link.addEventListener('mousemove', () => {
+    mouseCursor.classList.add('link-grow');
+    link.classList.add('hovered-link');
+  });
+  link.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove('link-grow');
+    link.classList.remove('hovered-link');
+  });
+});
